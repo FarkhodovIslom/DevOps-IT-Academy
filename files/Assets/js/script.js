ea8155ce24404dc8927699ce01d2d494bb1299d3
Farkhodov Islom <111051883+FarkhodovIslom@users.noreply.github.com>
2024-05-13 13:05:25 +0500

    v2.2.1

--- files/Assets/js/script.js
+++ files/Assets/js/script.js
@@ -1,19 +1,28 @@
 // Loader 
-let loaderContainer = document.querySelector(".loader-container");
-function getRandomNumber(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-var numbers = [3, 4, 5];
-var randomIndex = getRandomNumber(0, numbers.length - 1);
-var randomNumber = numbers[randomIndex] * 1000;
-let loadingTimer = setTimeout(() => {
+const loaderContainer = document.querySelector(".loader-container");
+// function getRandomNumber(min, max) {
+//   min = Math.ceil(min);
+//   max = Math.floor(max);
+//   return Math.floor(Math.random() * (max - min + 1)) + min;
+// }
+// var numbers = [3, 4, 5];
+// var randomIndex = getRandomNumber(0, numbers.length - 1);
+// var randomNumber = numbers[randomIndex] * 1000;
+// let loadingTimer = setTimeout(() => {
+//     loaderContainer.classList.add("loader-closing");
+//     setTimeout(() => {
+//       loaderContainer.style.display = "none";
+//     }, 300)
+// }, randomNumber)
+
+window.onload = () => {
     loaderContainer.classList.add("loader-closing");
     setTimeout(() => {
       loaderContainer.style.display = "none";
-    }, 300)
-}, randomNumber)
+    }, 350)
+}
+
+
 
 
 // Navbar
