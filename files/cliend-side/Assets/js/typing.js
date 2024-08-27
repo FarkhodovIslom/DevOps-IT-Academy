54d6b92893ef9c08b11739f62fd1fd71e5d2d4cb
Islom Farkhodov <farkhodov@Isloms-MacBook-Pro.local>
2024-08-27 18:53:13 +0500

    v3.0 update, Changed overall design

--- files/cliend-side/Assets/js/typing.js
+++
@@ -1,4 +0,0 @@
-var typed = new Typed('.auto-typing', {
-    strings: ["DevOps"],
-    typeSpeed: 100,
-});
--- files/server-side/certificate.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const certificateSchema = new mongoose.Schema({
-    name: String,
-    file: Buffer
-});
-
-const Certificate = mongoose.model('Certificate', certificateSchema);
-module.exports = Certificate;
